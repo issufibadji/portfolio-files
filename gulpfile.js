740344cddcfbfc1240a23f7ba08c0010eee17123
Thalyson Rodrigues <thalysonrodrigues.dev@gmail.com>
2020-03-22 14:54:42 -0400

    atualizar tarefa de minificação json
    minifica todos arquivos json de subdiretorios de data e exclui a criacao de qualquer arquivo dentro de compress que nao seja os gerados pela minificacao.

--- gulpfile.js
+++ gulpfile.js
@@ -9,7 +9,7 @@ gulp.task('image', () => {
 });
 
 gulp.task('jsonminify', () => {
-  return gulp.src('./data/**')
+  return gulp.src(['./data/**/*.json', '!data/compress/**']) // minifica todos arquivos json de subdiretorios de data e exclui a criacao de qualquer arquivo dentro de compress que nao seja os gerados pela minificacao.
     .pipe(jsonminify())
     .pipe(gulp.dest('./data/compress'));
 });
